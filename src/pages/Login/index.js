f94dc116f5a6b1475b1cf6375532ac694779df17
dungvudinh <anhkho881@gmail.com>
2024-05-27 14:48:58 +0700

    fix conflict

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import styles from './Login.module.scss';
 import classNames from 'classnames/bind';
-import { TextField, Button } from '@mui/material';
+import { TextField, Button, Dialog, DialogContent, DialogActions, DialogContentText, DialogTitle } from '@mui/material';
 import axios from 'axios';
 import CustomAlert from '../../components/CustomAlert';
 import OtpInput from 'react-otp-input';
@@ -18,11 +18,17 @@ function Login() {
         phone_number: '', 
         password: ''
     })
+    const [newPassword, setNewPassword] = useState({
+        new_password: '', 
+        new_confirm_password: ''
+    })
     const [showAlert, setShowAlert] = useState();
     const [alertMessage, setAlertMessage] = useState({});
     const [openInputOTP, setOpenInputOTP] = useState(false);
     const [otp, setOTP] = useState('');
     const [isLoading, setIsLoading] =useState(false);
+    const [openDialog, setOpenDialog] = useState(false);
+    const [isVerifyOTP ,setIsVerifyOTP] = useState(false);
     const navigate = useNavigate();
     axios.defaults.withCredentials = true;
     axios.defaults.headers.post['Content-Type'] = 'application/json';
@@ -63,9 +69,7 @@ function Login() {
         })
    },[])
     
-    const onCaptchaVerify = ()=>{
-        ;
-    }
+    
     const handleSendOTP = async ()=>{
         if(!accountInfo.phone_number){
             setAlertMessage({status:'error', message: 'Vui lòng nhập số điện thoại'});
@@ -94,14 +98,13 @@ function Login() {
     
     const handleVerifyOTP = ()=>{
        setIsLoading(true);
-       console.log(otp);
-       console.log( window.confirmationResult)
        if(window.confirmationResult){
            window.confirmationResult.confirm(otp)
            .then(async(res)=>{
                 setAlertMessage({status:'success', message:'Xác thực OTP thành công'});
                 setShowAlert(true);
                 setIsLoading(false)
+                setIsVerifyOTP(true);
            })
            .catch((error)=>{
                 setAlertMessage({status:'error', message:'Xác thực OTP thất bại'});
@@ -114,6 +117,29 @@ function Login() {
             setShowAlert(true);
             setIsLoading(false)
        }
+    }
+    const handleResetPassword =async  ()=>{
+        try 
+        {
+            var newObj = {
+                phone_number: accountInfo.phone_number, 
+                new_password: newPassword.new_password, 
+                new_confirm_password: newPassword.new_confirm_password
+            }
+            const result = await instance.post('/reset-password',newObj);
+            setAlertMessage({status:result.data.status, message: result.data.message});
+            setShowAlert(true);
+           
+            if(result.data.status === 'success'){
+                setTimeout(()=>{
+                    window.location.reload();
+                }, 1000)
+            }
+        }
+        catch(error){
+            console.log(error);
+        }
+       
     }
     return ( 
         <div className={cx('wrapper')}>
@@ -145,7 +171,6 @@ function Login() {
                             value={otp}
                             onChange={setOTP}
                             numInputs={6}
-                            // renderSeparator={<span>-</span>}
                             containerStyle={{display:'flex', justifyContent:'space-between', marginTop:'10px'}}
                             inputStyle={{border:'none', borderBottom:'1px solid #666', width:'40px', height:'35px'}}
                             renderInput={(props) => <input {...props}  inputType="number" shouldAutoFocus={true}/>}
@@ -153,6 +178,25 @@ function Login() {
                         <p style={{color:'#666', marginTop:'10px'}}>Mã OTP đã được gửi về số điện thoại {accountInfo.phone_number}</p>
                     </div>   
                 )}
+                {isVerifyOTP &&
+                (
+                    <>
+                    <div className={cx('form-group')}>
+                        <div className={cx('input-group')}>
+                            <TextField id="outlined-basic" label="Mật khẩu mới" variant="standard" fullWidth={true} size='small' name='new_password' type="text"
+                            onChange={e=>setNewPassword({...newPassword, new_password: e.target.value})} required/>
+                        </div>
+                    </div>
+                    <div className={cx('form-group')}>
+                        <div className={cx('input-group')}>
+                            <TextField id="outlined-basic" label="Nhập mật khẩu mới" variant="standard" fullWidth={true} size='small' name='new_confirm_password' type="text"
+                            onChange={e=>setNewPassword({...newPassword, new_confirm_password: e.target.value})} required/>
+                        </div>
+                    </div>
+                    </>
+                )
+                }
+               
                 <div className={cx('form-group')}>
                     {!openInputOTP ?(
                         <Button variant="contained" color="success" fullWidth={true} size='small' onClick={handleLogin}>
@@ -161,9 +205,14 @@ function Login() {
                     )
                     :
                     (
-                        <Button variant="contained" color="success" fullWidth={true} size='small' onClick={handleVerifyOTP}>
+                        !isVerifyOTP ? 
+                        <Button variant="contained" color="success" fullWidth={true} size='small' onClick={handleVerifyOTP} disabled={otp !== '' ? false : true}>
                             {isLoading && (<FontAwesomeIcon icon={faCircleNotch} style={{marginRight:'10px'}} className={cx('loading')}/>)}
                             Xác thực OTP
+                        </Button>
+                        : 
+                        <Button variant="contained" color="success" fullWidth={true} size='small' onClick={handleResetPassword} disabled={otp !== '' ? false : true}>
+                            Xác nhận
                         </Button>
                     )
                     }
@@ -173,8 +222,46 @@ function Login() {
             </div>
         </div>
         {showAlert && <CustomAlert alert={alertMessage} open={showAlert} onClose={()=>setShowAlert(false)}/>}
+        {/* <SetNewPasswordDialog open={openDialog} handleClose={()=>setOpenDialog(false)}/> */}
         </div>
      );
 }
 
+
+function SetNewPasswordDialog({open, handleClose}){
+  
+  return (
+    <>
+    
+      <Dialog
+        open={true}
+        onClose={handleClose}
+        
+      >
+        <DialogTitle>Subscribe</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            To subscribe to this website, please enter your email address here. We
+            will send updates occasionally.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            required
+            margin="dense"
+            id="name"
+            name="email"
+            label="Email Address"
+            type="email"
+            fullWidth
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button type="submit">Subscribe</Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  )
+}
 export default Login;
